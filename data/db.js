e609b1b1546a7b4d52f1c2ee2360602abc70fd79
LuigiCasale95 <luigicasale95@gmail.com>
2025-10-31 15:47:32 +0100

    aggiunta del .env per rogliere dati personali

--- data/db.js
+++ data/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 /* usiamo il metodo di creazione oggetto di connessione */
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Luigicasale95',
-    database: 'movies_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 /* tramite metodo connect avvia connessione */
